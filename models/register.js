4a712305b3fc3bf6fd1c2a63a9dc5d57350b8fda
sneha-angularminds <sneha@angularminds.in>
2022-05-01 17:28:04 +0530

    change password api added

---
+++ models/register.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const Scehma = mongoose.Schema;
+
+const bcrypt = require("bcryptjs");
+
+const registerSchema = new Scehma({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: [emailValidator, "incorrect email field"],
+  },
+  password: {
+    type: String,
+    required: true,
+    // validate: [passwordValidator, "incorrect password field"],
+  },
+});
+
+function emailValidator(value) {
+  return /^.+@.+\..+$/.test(value);
+}
+
+// function passwordValidator(value) {
+//   return /^.+@.+\..+$/.test(value);
+// }
+registerSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(this.password, salt);
+    this.password = passwordHash;
+    next();
+  } catch (err) {
+    next(error);
+  }
+});
+
+registerSchema.methods.isPasswordValid = async function (value) {
+  try {
+    return await bcrypt.compare(value, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+module.exports = mongoose.model("user", registerSchema);
